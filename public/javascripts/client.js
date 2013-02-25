c6821e9a05a0a4f81f87e53a7c7b9f4444b11289
yamauchi <k.yamauchi@core-tech.jp>
2013-02-25 18:10:21 +0900

    userId認識
    撃墜メッセージ追加

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,26 +1,54 @@
+// TODO: 「var a = b | c」表記は、左側がfalseだったら右側を代入するの略記か調べる
+
 jQuery(function($) {
+	
+	/**************************/
+	/*   各種ハンドラを設定   */
+	/**************************/
 	"use strict";
+	// socket.ioにアクセスしてオブジェクトを取得
 	var _socket = io.connect('http://'+location.host+'/');
+	
+	// 自分以外のプレイヤーの座標情報を、key(userId)を基に保持する変数
 	var _userMap = {};
+	
+	// 自分以外の弾丸の座標情報を、key(userId)を基に保持する変数
 	var _bulletMap = {};
-	_socket.on('player-update',function(data){
+	
+	// サーバーの、プレイヤーの座標アップデート「socket.broadcast.json.emit()」を監視
+	_socket.on('player-update',function(data) {
 		var user;
-		if(_userMap[data.userId] === undefined){
-			console.log('create user '+data.userId , data);
-			user = {x:0,y:0,v:0,rotate:0,userId:data.userId};
-			user.element = $('<img src="/images/unit.png" class="player" />')
-				.attr('data-user-id',user.userId);
+		if(_userMap[data.userId] === undefined) {
+			console.log('create user ' + data.userId , data);
+
+			user = {
+				x:      0,
+				y:      0,
+				v:      0,
+				rotate: 0,
+				userId: data.userId
+			};
+
+			user.element = $('<span class="player"><img src="/images/unit.png" />' + data.userId + '</span>')
+				.attr('data-user-id', user.userId);
 			$('body').append(user.element);
 			_userMap[data.userId] = user;
-			
-			var bullet = {x:-100,y:-100,v:0,rotate:0,userId:data.userId};
+
+			var bullet = {
+				x:      -100,
+				y:      -100,
+				v:      0,
+				rotate: 0,
+				userId: data.userId
+			};
+
 			bullet.element = $('<img src="/images/bullet.png" class="bullet" />')
 				.attr('data-user-id',user.userId);
 			$('body').append(bullet.element);
 			
 			_bulletMap[data.userId] = bullet;
 			
-		}else{
+		} else {
 			user = _userMap[data.userId];
 		}
 		user.x = data.data.x;
@@ -29,7 +57,9 @@ jQuery(function($) {
 		user.v = data.data.v;
 		updateCss(user);
 	});
-	_socket.on('bullet-create',function(data){
+
+	// サーバーの、弾丸生成イベント「socket.broadcast.json.emit()」を監視
+	_socket.on('bullet-create',function(data) {
 		var bullet = _bulletMap[data.userId];
 		if(bullet !== undefined){
 			bullet.x = data.data.x;
@@ -39,7 +69,8 @@ jQuery(function($) {
 		}
 	});
 	
-	_socket.on('disconnect-user',function(data){
+	// サーバーの、切断イベント「socket.broadcast.json.emit()」を監視
+	_socket.on('disconnect-user',function(data) {
 		var user = _userMap[data.userId];
 		if(user !== undefined){
 			user.element.remove();
@@ -50,88 +81,160 @@ jQuery(function($) {
 		}
 		
 	});
-	
-	
+
+	// サーバーの、プレーヤーが撃墜されたお知らせイベント「socket.broadcast.json.emit()」を監視
+	_socket.on('inform-otherUnitBroken', function(data) {
+
+		console.log(data);
+		$('body').prepend('<div id="message">ID：' + data.userId + ' が撃墜されたよ</div>');
+		$('#message').animate({'top': '0'}).delay(2000).animate({'top': '-50px'}, function(){
+			$(this).remove();
+		});
+
+	});
+
+
+	/***********************************************/
+	/*   メインプログラム (ページロード時に走査)   */
+	/***********************************************/
 	var _keyMap = [];
-	var _player = {x:Math.random()*1000|0,y:Math.random()*500|0,v:0,rotate:0,element:$('#my-player')};
-	var _bullet = {x:-100,y:-100,v:0,rotate:0,element:$('#my-bullet')};
 	
-	var updatePosition = function(unit){
-		unit.x += unit.v* Math.cos(unit.rotate * Math.PI /180);
-		unit.y += unit.v* Math.sin(unit.rotate * Math.PI /180);
+	// プレーヤーの座標位置
+	var _player = {
+		x:       Math.random() * 1000 | 0,
+		y:       Math.random() * 500 | 0,
+		v:       0,
+		rotate:  0,
+		element: $('#my-player')
 	};
-	var updateCss = function(unit){
+	
+	// 弾丸の座標位置
+	var _bullet = {
+		x:       -100,
+		y:       -100,
+		v:       0,
+		rotate:  0,
+		element: $('#my-bullet')
+	};
+	
+	// 座標をアップデートする関数 (数値のみ)
+	var updatePosition = function(unit) {
+		unit.x += unit.v * Math.cos(unit.rotate * Math.PI / 180);
+		unit.y += unit.v * Math.sin(unit.rotate * Math.PI / 180);
+	};
+	
+	// 座標をアップデートする関数 (CSSを操作し、ブラウザ上の表示を変える)
+	var updateCss = function(unit) {
 		unit.element.css({
-			left:unit.x|0+'px',
-			top:unit.y|0 + 'px',
-			transform:'rotate('+unit.rotate+'deg)'
+			left:      unit.x | 0 + 'px',
+			top:       unit.y | 0 + 'px',
+			transform: 'rotate(' + unit.rotate + 'deg)'
 		});
 	};
-	//メインループ
-	var f = function(){
-		if(_keyMap[37] === true){
-			//left
-			_player.rotate -= 3;
-		}
-		if(_keyMap[38] === true){
-			//up
-			_player.v += 0.5;
-		}
-		if(_keyMap[39] === true){
-			//right
-			_player.rotate += 3;
-		}
-		if(_keyMap[40] === true){
-			//down
-			_player.v -= 0.5;
-		}
+	
+	// メインループ (ひたすら呼び出されて回り続ける)
+	var mainfunc = function() {
+
+		// プレイヤーor弾丸の位置情報を修正
+		// ループ中にキーボードが押されて、変数の値が変わって座標も変わる仕組み
+		
+		// キーボード「←」
+		if(_keyMap[37] === true) _player.rotate -= 3;
+		
+		// キーボード「↑」
+		if(_keyMap[38] === true) _player.v += 0.5;
+		
+		// キーボード「→」
+		if(_keyMap[39] === true) _player.rotate += 3;
+		
+		// キーボード「↓」
+		if(_keyMap[40] === true) _player.v -= 0.5;
+		
+		// キーボード「space」
 		if(_keyMap[32] === true && _isSpaceKeyUp){
-			//space
 			_isSpaceKeyUp = false;
 			_bullet.x = _player.x +20;
 			_bullet.y = _player.y +20;
 			_bullet.rotate = _player.rotate;
 			_bullet.v = Math.max(_player.v + 3,3);
-			_socket.emit('bullet-create',{
-				x:_bullet.x|0,
-				y:_bullet.y|0,
-				rotate:_bullet.rotate|0,
-				v:_bullet.v
+			
+			// 【イベント発生】弾丸生成イベントを発生させ、サーバーに弾丸の位置情報を渡す
+			_socket.emit('bullet-create', {
+				x:      _bullet.x | 0,
+				y:      _bullet.y | 0,
+				rotate: _bullet.rotate | 0,
+				v:      _bullet.v
 			});
 		}
+		
+		// スピード調整のようなもの（ブレーキ？）
 		_player.v *= 0.95;
+		
+		// プレイヤーの座標をアップデートする (数値のみ)
 		updatePosition(_player);
-		var w_width = $(window).width();
-		var w_height =$(window).height();
+		
+		// ウィンドウのサイズを取得
+		var w_width  = $(window).width();
+		var w_height = $(window).height();
+		
 		if(_player.x < -50){ _player.x = w_width;}
 		if(_player.y < -50){ _player.y = w_height;}
 		if(_player.x > w_width){_player.x = -50;}
 		if(_player.y > w_height){_player.y = -50;}
 		
+		
 		updatePosition(_bullet);
+		
+		// 自分以外の弾丸の座標判定
 		for(var key in _bulletMap){
+
 			var bullet = _bulletMap[key];
 			updatePosition(bullet);
 			updateCss(bullet);
-			if(_player.x < bullet.x && bullet.x <_player.x + 50 &&
-			_player.y < bullet.y && bullet.y <_player.y + 50){
+			
+			// 飛んでいる弾丸の座標がプレイヤーと被ったら、被弾したとしてgameover画面へ遷移する
+			if(_player.x < bullet.x &&
+			   bullet.x <_player.x + 50 &&
+			   _player.y < bullet.y &&
+			   bullet.y <_player.y + 50) {
+				
+				// 【イベント発生】プレーヤーが撃墜されたお知らせイベントを発生させる yamauchi
+				_socket.emit('inform-otherUnitBroken', {});
 				location.href = '/gameover';
 			}
 		}
+		
+		// プレイヤー・弾丸の座標をアップデートする (ブラウザ上の表示を変える)
 		updateCss(_bullet);
 		updateCss(_player);
-		_socket.emit('player-update',{x:_player.x|0,y:_player.y|0,rotate:_player.rotate|0,v:_player.v});
-		setTimeout(f,30);
+		
+		// 【イベント発生】プレイヤーの座標アップデートイベントを発生させ、サーバーに弾丸の位置情報を渡す
+		_socket.emit('player-update', {
+			x:      _player.x | 0,
+			y:      _player.y | 0,
+			rotate: _player.rotate | 0,
+			v:      _player.v
+		});
+		
+		// mainfunc()を再帰的に呼び出す
+		setTimeout(mainfunc, 30);
 	};
+
 	var _isSpaceKeyUp = true;
-	setTimeout(f,30);
-	$(window).keydown(function(e){
+
+	// mainfunc()の初回起動
+	setTimeout(mainfunc, 30);
+	
+	// キーボードのイベントハンドラを設定
+	$(window).keydown(function(e) {
 		_keyMap[e.keyCode] = true;
 	});
-	$(window).keyup(function(e){
+
+	$(window).keyup(function(e) {
 		if(e.keyCode === 32){
 			_isSpaceKeyUp = true;
 		}
 		_keyMap[e.keyCode] = false;
 	});
+
 });
--- server.js
+++ server.js
@@ -36,18 +36,36 @@ server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-var io = require('socket.io').listen(server,{'log level':1});
+var io = require('socket.io').listen(server,{'log level': 1});
 var _userId = 0;
-io.sockets.on('connection',function(socket){
+
+// socket.ioへの接続（ゲームスタート）イベント監視
+io.sockets.on('connection', function(socket){
+	
+	// クライアントをIDで判別し、
+	// Socket.IOのセッションとして扱われるsocket.handshake.userIdに格納
+	console.log('game start! - userID : ' + _userId);
 	socket.handshake.userId = _userId;
 	_userId ++;
-	socket.on('player-update',function(data){
-		socket.broadcast.json.emit('player-update',{userId:socket.handshake.userId,data:data});
+	
+	// プレイヤーの座標アップデートイベント監視
+	socket.on('player-update', function(data) {
+		socket.broadcast.json.emit('player-update',{userId:socket.handshake.userId, data:data});
 	});
-	socket.on('bullet-create',function(data){
-		socket.broadcast.json.emit('bullet-create',{userId:socket.handshake.userId,data:data});
+
+	// 弾丸の生成イベント監視
+	socket.on('bullet-create', function(data) {
+		socket.broadcast.json.emit('bullet-create',{userId:socket.handshake.userId, data:data});
 	});
-	socket.on('disconnect',function(){
+
+	// 切断イベント(既存の切断イベント)監視
+	socket.on('disconnect', function() {
 		socket.broadcast.json.emit('disconnect-user',{userId:socket.handshake.userId});
 	});
+
+	// プレーヤーが撃墜されたお知らせイベント監視 yamauchi
+	socket.on('inform-otherUnitBroken', function(data) {
+		socket.broadcast.json.emit('inform-otherUnitBroken', {userId:socket.handshake.userId});
+	});
+
 });
